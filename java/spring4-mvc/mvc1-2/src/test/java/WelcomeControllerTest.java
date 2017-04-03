be83f668ac93ab29201b8c6e09f2dccfa6172f06
wsunccake <wsunccake@hotmail.com>
2017-04-03 09:27:32 +0800

    Add mvc1-2 unittest

--- java/spring4-mvc/mvc1-2/src/test/java/WelcomeControllerTest.java
+++ java/spring4-mvc/mvc1-2/src/test/java/WelcomeControllerTest.java
@@ -1,24 +1,46 @@
 package mypkg;
 
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import org.junit.Test;
 
-//import mypkg.WelcomeController;
 import org.springframework.test.web.servlet.MockMvc;
 
 public class WelcomeControllerTest {
+	private WelcomeController controller = new WelcomeController();
+	private MockMvc mockMvc = standaloneSetup(controller).build();
+
 	@Test
 	public void testHomePage() throws Exception {
-		WelcomeController controller = new WelcomeController();
-		assertEquals("home", controller.home());
+		mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("name", "Spring MVC"));
 
-		MockMvc mockMvc = standaloneSetup(controller).build();
-		mockMvc.perform(get("/")).andExpect(view().name("home"));
 		mockMvc.perform(get("/homepage")).andExpect(view().name("home"));
-
 	}
+
+	@Test
+	public void testHiPage() throws Exception {
+        String testName = "abc";
+		mockMvc.perform(get("/hi?name=" + testName))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("msg", "hi"))
+                .andExpect(model().attribute("name", testName));
+	}
+
+    @Test
+    public void testHelloPage() throws Exception {
+        String testName = "xyz";
+        mockMvc.perform(get("/hello/" + testName))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("msg", "Hello"))
+                .andExpect(model().attribute("name", testName));
+    }
 }
